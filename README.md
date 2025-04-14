296f6bc2c28eef901ef7f34b3970d7c077c3ecf5
unknown <Siphomoloto@gmail.com>
2025-04-15 01:25:13 +0200

    Official-Initial Commit: starter template with expressjs

---
+++ README.md
@@ -0,0 +1,3 @@
+# AudioTOTube
+
+An express backend that will allow me to create a video and publish it to youtube by providing an audio file and an image.
---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
