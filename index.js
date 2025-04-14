7bad0296113c026acf131e00be2766999f73a308
unknown <Siphomoloto@gmail.com>
2025-04-15 01:46:37 +0200

    Added dotenv functionality

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // index.js
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.VERCEL_URL || "http://localhost:";
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  console.log(process.env)
+  res.send('Running host: ', HOST, ' port; ', PORT, ' from env: ', process.env);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at ${HOST}${PORT}`);
 });
